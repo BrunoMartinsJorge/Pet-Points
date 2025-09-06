441ba9cd9e1e927d81fd96dd52da93f0a16356a4
Bruno <rexon300008@gmail.com>
2025-09-06 20:27:18 -0300

    feat: Adicionar Back(Spring 21)

---
+++ back/src/test/java/br/com/petpoints/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.petpoints.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
